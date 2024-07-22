b19d9aac3a1496dd88b1c9b2d5f11f0b42817b83
cassiano belniak <cassiano.belniak@gmail.com>
2024-07-21 21:22:32 -0300

    feat: added layout

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,5 +1,31 @@
+<script setup>
+import LateralBar from './components/LateralBar/LateralBar.vue'
+</script>
 <template>
-  <main>
-    <RouterView />
-  </main>
+  <q-layout view="lHr lpR lFr">
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" behavior="desktop" elevated>
+      <LateralBar />
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
 </template>
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
--- src/renderer/components/LateralBar/LateralBar.vue
+++ src/renderer/components/LateralBar/LateralBar.vue
@@ -1,3 +1,3 @@
 <template>
-    content
+    content goes here
 </template>
