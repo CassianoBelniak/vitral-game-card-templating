a9f28ab0dec3890422545224af1680246c439f23
cassiano belniak <cassiano.belniak@gmail.com>
2024-07-22 21:06:44 -0300

    feat: adjusts to layout

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,31 +1,4 @@
-<script setup>
-import LateralBar from './components/LateralBar/LateralBar.vue'
-</script>
 <template>
-  <q-layout view="lHr lpR lFr">
-
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" behavior="desktop">
-      <LateralBar />
-    </q-drawer>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-
-  </q-layout>
+  <router-view />
 </template>
-<script>
-import { ref } from 'vue'
-export default {
-  setup() {
-    const leftDrawerOpen = ref(false)
 
-    return {
-      leftDrawerOpen,
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-}
-</script>
---
+++ src/renderer/pages/EditCardPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <q-layout view="lHr lpR lFr">
+
+        <q-drawer show-if-above side="left" behavior="desktop">
+
+        </q-drawer>
+
+        <q-page-container>
+
+        </q-page-container>
+
+    </q-layout>
+</template>
