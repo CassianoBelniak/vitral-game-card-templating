bb5a7d4c87adc628aaf6a7d2bf3655fc8821c567
cassiano belniak <cassiano.belniak@gmail.com>
2024-07-24 23:16:22 -0300

    chore: added todos

--- src/main/ipc-events/get-config.ts
+++ src/main/ipc-events/get-config.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
+//TODO: remove these comments
 import { configStore } from '../configs/config-store.js'
 
 export function getConfig(
