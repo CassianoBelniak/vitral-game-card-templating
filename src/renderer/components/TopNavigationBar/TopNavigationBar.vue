8b76a42dd33ec4f97fe863ac15559d6d804b5104
cassiano belniak <cassiano.belniak@gmail.com>
2024-07-25 21:27:47 -0300

    feat: improved navigation

--- src/renderer/components/TopNavigationBar/TopNavigationBar.vue
+++ src/renderer/components/TopNavigationBar/TopNavigationBar.vue
@@ -1,12 +1,19 @@
+<script setup>
+import { fileStore } from '../../stores/file-store.ts';
+
+const currentProject = fileStore.projectName
+const isNavigationDisabled = !currentProject
+
+</script>
 <template>
     <div class="q-pa-md">
         <q-toolbar>
             <q-btn push label="Home" to="/" />
-            <q-btn push label="Configs" to="/configs" />
-            <q-btn push label="Gallery" to="gallery" />
-            <q-btn push label="Templates" to="/templates" />
-            <q-btn push label="Assets" to="/assets" />
-            <q-btn push label="Exporting" to="/export" />
+            <q-btn push label="Configs" to="/configs" :disable="isNavigationDisabled" />
+            <q-btn push label="Gallery" to="gallery" :disable="isNavigationDisabled" />
+            <q-btn push label="Templates" to="/templates" :disable="isNavigationDisabled" />
+            <q-btn push label="Assets" to="/assets" :disable="isNavigationDisabled" />
+            <q-btn push label="Exporting" to="/export" :disable="isNavigationDisabled" />
         </q-toolbar>
     </div>
 </template>
