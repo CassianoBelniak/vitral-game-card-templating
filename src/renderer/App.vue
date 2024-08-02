3592aab19c67ad97cff86124093e79d7c031cf57
cassiano belniak <cassiano.belniak@gmail.com>
2024-08-01 21:38:25 -0300

    feat: updated example project

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import './stores/images-store.js'
 import './stores/fonts-store.js'
+import './stores/templates-store.js'
+import './stores/cards-store.js'
 </script>
 
 <template>
