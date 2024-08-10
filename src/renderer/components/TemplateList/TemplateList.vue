2d6bc85d7fabdbb477bd56845ddecf4da68deef4
cassiano belniak <cassiano.belniak@gmail.com>
2024-08-09 21:21:37 -0300

    feat: added preview to template list

--- src/renderer/components/TemplateList/TemplateList.vue
+++ src/renderer/components/TemplateList/TemplateList.vue
@@ -12,8 +12,13 @@ const goToTemplateEdit = (templateName: string) => {
     <div class="row">
         <div class="card-container" v-for="(template, index) in templatesStore.templates" :key="index">
             <q-card class="template-card" @click="goToTemplateEdit(template.name)">
+                <q-card-section class="image">
+                    <Fit>
+                        <RenderedTemplate class="template" :template="template" />
+                    </Fit>
+                </q-card-section>
                 <q-card-section>
-                    <div class="text-subtitle2">{{ template.name }}</div>
+                    <div>{{ template.name }}</div>
                 </q-card-section>
             </q-card>
         </div>
@@ -24,6 +29,10 @@ const goToTemplateEdit = (templateName: string) => {
     margin-bottom: 30px;
 }
 
+.image {
+    width: 100%;
+    height: 50%;
+}
 .template-card {
     height: 150px;
     margin-right: 20px;
