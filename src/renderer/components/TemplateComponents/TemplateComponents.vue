1a956f41645347082de5df401d93af6e9622027d
Cassiano Belniak <cassiano.belniak@gmail.com>
2024-08-13 21:12:55 -0300

    fix: fixed component list overflowing

--- src/renderer/components/TemplateComponents/TemplateComponents.vue
+++ src/renderer/components/TemplateComponents/TemplateComponents.vue
@@ -5,9 +5,11 @@ const model = defineModel<Template>({ default: new Template() })
 
 </script>
 <template>
-    <q-card class="p-2 my-2">
-        <div>General</div>
-        <q-input v-model="model.name" label="Name" dense />
-    </q-card>
-    <ComponentList v-model="model" />
+    <q-scroll-area class="h-full">
+        <q-card class="p-2 my-2">
+            <div>General</div>
+            <q-input v-model="model.name" label="Name" dense />
+        </q-card>
+        <ComponentList v-model="model" />
+    </q-scroll-area>
 </template>
