a63d11e0f24641d84a576e7cf30e100a67a21427
cassiano belniak <cassiano.belniak@gmail.com>
2024-09-13 19:40:49 -0300

    feat: added ratio to image sizes

--- src/renderer/classes/ComponentImage.ts
+++ src/renderer/classes/ComponentImage.ts
@@ -14,6 +14,20 @@ export interface ComponentImageJSON extends ComponentJSON {
     name: string
 }
 
+function getRatio(width: string, height: string, image_width: string, image_height: string) {
+    if (width) {
+        const value = new Parser(width).toPixels()
+        const image_value = new Parser(image_width).toPixels()
+        return image_value / value
+    }
+    if (height) {
+        const value = new Parser(height).toPixels()
+        const image_value = new Parser(image_height).toPixels()
+        return image_value / value
+    }
+    return 1
+}
+
 export class ComponentImage extends Component {
     type = 'image'
     width: string = ''
@@ -45,29 +59,23 @@ export class ComponentImage extends Component {
             width: String(image?.image?.width),
             height: String(image?.image?.height),
         }
+        const ratio = getRatio(this.width, this.height, dimensions.width, dimensions.height)
+        let width = new Parser(this.width).variables(variables).default(dimensions.width).toPixels()
+        let height = new Parser(this.height).variables(variables).default(dimensions.height).toPixels()
+        if (this.height && !this.width) {
+            width /= ratio
+        }
+        if (!this.height && this.width) {
+            height /= ratio
+        }
         return {
-            width: new Parser(this.width)
-                .variables(variables)
-                .default(dimensions.width)
-                .toPixels(),
-            height: new Parser(this.height)
-                .variables(variables)
-                .default(dimensions.height)
-                .toPixels(),
+            width,
+            height,
             x: new Parser(this.x).variables(variables).default('0').toPixels(),
             y: new Parser(this.y).variables(variables).default('0').toPixels(),
-            offsetX: new Parser(this.offsetX)
-                .variables(variables)
-                .default('0')
-                .toPixels(),
-            offsetY: new Parser(this.offsetY)
-                .variables(variables)
-                .default('0')
-                .toPixels(),
-            rotation: new Parser(this.rotation)
-                .variables(variables)
-                .default('0')
-                .toNumber(),
+            offsetX: new Parser(this.offsetX).variables(variables).default('0').toPixels(),
+            offsetY: new Parser(this.offsetY).variables(variables).default('0').toPixels(),
+            rotation: new Parser(this.rotation).variables(variables).default('0').toNumber(),
             name,
             context: this.context,
         }
