4297f01213bf48ae840e7af5376fab90b6006a9f
cassiano belniak <cassiano.belniak@gmail.com>
2024-09-14 12:18:07 -0300

    fix: fixed font removing

--- src/renderer/helpers/manage-font.ts
+++ src/renderer/helpers/manage-font.ts
@@ -1,26 +1,13 @@
 export function registerFont(family: string, data: string) {
-    console.log(family)
     const newFont = new FontFace(family, `url(data:font/ttf;base64,${data})`)
     newFont.load().then((font) => {
         document.fonts.add(font)
     })
-
-    // const newStyle = document.createElement('style')
-    // newStyle.appendChild(
-    //     document.createTextNode(`
-    // @font-face {
-    //     font-family: '${family}';
-    //     src: url(data:font/ttf;base64,${data}) format('truetype');
-    // }
-    // `),
-    // )
-
-    // document.head.appendChild(newStyle)
 }
 
 export function unregisterFont(family: string) {
     for (const fontFace of document.fonts) {
-        if ((fontFace.family = family)) {
+        if (fontFace.family === family) {
             document.fonts.delete(fontFace)
         }
     }
