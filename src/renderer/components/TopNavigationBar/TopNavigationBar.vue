e6e42da591a7443fdc4f9c875115ffde63e8a71d
cassiano belniak <cassiano.belniak@gmail.com>
2024-10-02 22:07:45 -0300

    feat: reordered top buttons

--- src/renderer/components/TopNavigationBar/TopNavigationBar.vue
+++ src/renderer/components/TopNavigationBar/TopNavigationBar.vue
@@ -1,22 +1,22 @@
 <script setup>
-import { projectConfigStore } from '../../stores/project-config-store';
+    import { projectConfigStore } from '../../stores/project-config-store';
 
-const currentProject = projectConfigStore.projectName
-const isNavigationDisabled = !currentProject
+    const currentProject = projectConfigStore.projectName
+    const isNavigationDisabled = !currentProject
 
 </script>
 <template>
     <div class="q-pa-md">
         <q-toolbar>
-            <q-btn class="button" no-caps push icon="home" label="Home" to="/" />
-            <q-btn class="button" no-caps push icon="settings" label="Configs" to="/configs"
-                :disable="isNavigationDisabled" />
+            <q-btn class="button" no-caps push icon="folder" label="Projects" to="/" />
             <q-btn class="button" no-caps push icon="photo" label="Cards" to="/cards" :disable="isNavigationDisabled" />
             <q-btn class="button" no-caps push icon="code" label="Templates" to="/templates"
                 :disable="isNavigationDisabled" />
             <q-btn class="button" no-caps push icon="burst_mode" label="Assets" to="/assets"
                 :disable="isNavigationDisabled" />
             <q-btn class="button" no-caps push icon="cloud_download" label="Exporting" to="/export"
+                :disable="isNavigationDisabled" />
+            <q-btn class="button" no-caps push icon="settings" label="Configs" to="/configs"
                 :disable="isNavigationDisabled" />
         </q-toolbar>
     </div>
