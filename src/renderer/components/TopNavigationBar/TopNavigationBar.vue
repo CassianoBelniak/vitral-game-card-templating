4a118b650f472e31b0096bd3809a056d861e4ff6
Cassiano Belniak <cassiano.belniak@gmail.com>
2026-04-18 14:35:26 -0300

    feat: improved navigation

--- src/renderer/components/TopNavigationBar/TopNavigationBar.vue
+++ src/renderer/components/TopNavigationBar/TopNavigationBar.vue
@@ -1,18 +1,61 @@
 <script setup>
+import { useRoute } from 'vue-router'
 import { projectConfigStore } from '../../stores/project-config-store'
+
+const route = useRoute()
 
 const currentProject = projectConfigStore.projectName
 const isNavigationDisabled = !currentProject
+
+const buttons = [
+    {
+        label: 'Projects',
+        route: '/projects',
+        icon: 'folder',
+    },
+    {
+        label: 'Cards',
+        route: '/cards',
+        icon: 'photo',
+    },
+
+    {
+        label: 'Templates',
+        route: '/templates',
+        icon: 'code',
+    },
+    {
+        label: 'Assets',
+        route: '/assets',
+        icon: 'burst_mode',
+    },
+    {
+        label: 'Exporting',
+        route: '/export',
+        icon: 'cloud_download',
+    },
+    {
+        label: 'Configs',
+        route: '/configs',
+        icon: 'settings',
+    },
+]
 </script>
 <template>
     <div class="q-pa-md">
         <q-toolbar>
-            <q-btn class="button" no-caps push icon="folder" label="Projects" to="/projects" />
-            <q-btn class="button" no-caps push icon="photo" label="Cards" to="/cards" :disable="isNavigationDisabled" />
-            <q-btn class="button" no-caps push icon="code" label="Templates" to="/templates" :disable="isNavigationDisabled" />
-            <q-btn class="button" no-caps push icon="burst_mode" label="Assets" to="/assets" :disable="isNavigationDisabled" />
-            <q-btn class="button" no-caps push icon="cloud_download" label="Exporting" to="/export" :disable="isNavigationDisabled" />
-            <q-btn class="button" no-caps push icon="settings" label="Configs" to="/configs" :disable="isNavigationDisabled" />
+            <q-btn
+                v-for="button in buttons"
+                :key="button.route"
+                class="button"
+                no-caps
+                push
+                :icon="button.icon"
+                :label="button.label"
+                :to="button.route"
+                :disable="isNavigationDisabled || button.route === route.path"
+                :color="route.path === button.route ? 'primary' : ''"
+            />
         </q-toolbar>
     </div>
 </template>
